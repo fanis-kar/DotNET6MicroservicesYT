ff8cf287bdeaf6578a4a230d0665fd44714e16bd
Theofanis Karamichalelis <fanis.karamichalelis@gmail.com>
2022-02-27 19:27:45 +0200

    add notes

--- PlatformService/Data/AppDbContext.cs
+++ PlatformService/Data/AppDbContext.cs
@@ -7,7 +7,6 @@ namespace PlatformService.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
         {
-
         }
 
         public DbSet<Platform> Platforms { get; set; }
