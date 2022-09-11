b6e0872d20cb9b92e0aeec6f2fbceb3f8d94b1b9
Theofanis Karamichalelis <fanis.karamichalelis@gmail.com>
2022-09-12 02:59:40 +0300

    Deploying SQL Server to Kubernetes. Updating our Platform Service to use SQL Server.

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -4,9 +4,20 @@ using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(opt => 
-    opt.UseInMemoryDatabase("InMem"));
+if (builder.Environment.IsProduction())
+{
+    Console.WriteLine("--> Using SqlServer Db");
 
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+       opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine("--> Using InMem Db");
+
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+       opt.UseInMemoryDatabase("InMem"));
+}
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
@@ -36,7 +47,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-PrepDb.PrepPopulation(app);
+PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 
 app.Run();
 
